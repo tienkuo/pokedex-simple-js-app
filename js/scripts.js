e72775fa8fd4c0db6eea212918673d029fd55a00
tienkuo <66206946+tienkuo@users.noreply.github.com>
2022-03-06 17:19:56 +0000

    Add Pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-alert('Hello World');
+let pokemonList = [];
 
-let favoriteFood = 'hot pot';
-document.write(favoriteFood);
+let pokemon1 = {
+  name: "Bulbasaur",
+  height: 0.7,
+  types: ['grass', 'poison']
+};
+
+let pokemon2 = {
+  name: "Moltres",
+  height: 2,
+  types: ['fire', 'flying']
+};
+
+let pokemon3 = {
+  name: "Dialga",
+  height: 5.4,
+  types: ['dragon', 'steel']
+};
+
+let pokemon4 =  {
+  name: "Qwilfish",
+  height: 0.5,
+  types: ['water', 'poison']
+};
+
+pokemonList = [pokemon1, pokemon2, pokemon3, pokemon4];
