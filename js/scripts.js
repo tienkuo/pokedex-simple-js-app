64085d1d17375f7cbc02b83a4ecba39eac59449b
tienkuo <66206946+tienkuo@users.noreply.github.com>
2022-03-07 13:50:19 +0000

    Correct array objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,25 @@
-let pokemonList = [];
-
-let pokemon1 = {
+let pokemonList = [
+  {
   name: "Bulbasaur",
   height: 0.7,
   types: ['grass', 'poison']
-};
+  },
 
-let pokemon2 = {
-  name: "Moltres",
-  height: 2,
-  types: ['fire', 'flying']
-};
+  {
+    name: "Moltres",
+    height: 2,
+    types: ['fire', 'flying']
+  },
 
-let pokemon3 = {
-  name: "Dialga",
-  height: 5.4,
-  types: ['dragon', 'steel']
-};
+  {
+    name: "Dialga",
+    height: 5.4,
+    types: ['dragon', 'steel']
+  },
 
-let pokemon4 =  {
-  name: "Qwilfish",
-  height: 0.5,
-  types: ['water', 'poison']
-};
-
-pokemonList = [pokemon1, pokemon2, pokemon3, pokemon4];
+  {
+    name: "Qwilfish",
+    height: 0.5,
+    types: ['water', 'poison']
+  }
+];
