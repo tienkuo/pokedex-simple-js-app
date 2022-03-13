52f74d677518055eddfa03796e50d2cc87b72fb8
tienkuo <66206946+tienkuo@users.noreply.github.com>
2022-03-13 15:21:19 +0000

    Update IIFE and forEach loop for pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,67 @@
-let pokemonList = [
-  {
-  name: "Bulbasaur",
-  height: 0.7,
-  types: ['grass', 'poison']
-  },
+//IIFE
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ['grass', 'poison']
+    },
 
-  {
-    name: "Moltres",
-    height: 2,
-    types: ['fire', 'flying']
-  },
+    {
+      name: "Moltres",
+      height: 2,
+      types: ['fire', 'flying']
+    },
 
-  {
-    name: "Dialga",
-    height: 5.4,
-    types: ['dragon', 'steel']
-  },
+    {
+      name: "Dialga",
+      height: 5.4,
+      types: ['dragon', 'steel']
+    },
 
-  {
-    name: "Qwilfish",
-    height: 0.5,
-    types: ['water', 'poison']
+    {
+      name: "Qwilfish",
+      height: 0.5,
+      types: ['water', 'poison']
+    }
+  ];
+
+  function add(pokemon) {
+    if (typeof pokemon === 'object'){
+      pokemonList.push(pokemon);
+    }
   }
-];
+
+  function filter(name) {
+    return pokemonList.filter(pokemonList => pokemonList.name === name);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    filter: filter,
+    getAll: getAll
+  };
+})();
 
 
-for (let i=0; i < pokemonList.length; i++) {
-  const pokeIndex = `${pokemonList[i].name} (height: ${pokemonList[i].height})`;
-  if (pokemonList[i].height > 5) {
+//updated forEach loop
+pokemonRepository.getAll().forEach(function(pokemon){
+  const pokeIndex = `${pokemon.name} (height: ${pokemon.height})`;
+  if (pokemon.height > 5) {
     document.write('<p>' + pokeIndex + ' - Wow, that\'s big!</p>');
   }else{
     document.write('<p>' + pokeIndex + '</p>');
   }
-}
+})
+
+//test to add a pokemon that is not an object
+// pokemonRepository.add("bird");
+// console.log(pokemonRepository.getAll())
+//test to add a pokemon that is a object
+// pokemonRepository.add({name: 'Blastoise', height: 1.6, types:'water'});
+// console.log(pokemonRepository.getAll())
+// console.log(pokemonRepository.filter('Dialga'))
